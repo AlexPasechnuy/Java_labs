eb1c5b16c6ea095247d9c771366765c8331385e8
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-09-29 22:26:58 +0300

    GenLib.swapGrp created. Need fix!

--- src/Lab1/GenLib/GenLib.java
+++ src/Lab1/GenLib/GenLib.java
@@ -1,13 +1,47 @@
 package Lab1.GenLib;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class GenLib {
+public class GenLib<T> {
     public static <T> T[] swapGroups(int length, T... t) {
         Object[] temp = new Object[t.length];
         System.arraycopy(t, 0, temp, t.length - length, length);
         System.arraycopy(t, t.length - length, temp, 0, length);
         return (T[]) temp;
+    }
+
+    public static<T> void swapGrp(T[] arr, int firstStart, int firstEnd, int secStart, int secEnd ){
+        if(firstStart < 0 || secEnd > arr.length || firstStart > firstEnd || secStart > secEnd || secStart < firstEnd){
+            //throw exception
+        }
+        int firstSize = firstEnd - firstStart;
+        int midSize = secStart - firstEnd;
+        int secSize = secEnd - secStart;
+
+        Object[] first = new Object[firstSize];
+        Object[] mid = new Object[midSize];
+        Object[] second = new Object[secSize];
+
+        System.arraycopy(arr, firstStart, first, 0 ,  firstSize);
+        System.arraycopy(arr, firstEnd, mid, 0 ,  midSize);
+        System.arraycopy(arr, secStart, second, 0 ,  secSize);
+
+        System.arraycopy(second, 0, arr, firstStart,  secSize);
+        System.arraycopy(mid, 0, arr, firstStart + secSize,  midSize);
+        System.arraycopy(first, 0, arr, firstStart + secSize + midSize,  secSize);
+    }
+
+    public static<T> void swapNeighb(){
+
+    }
+
+    public static<T> void insertIn(){
+
+    }
+
+    public static<T> void replOtherArr(){
+
     }
 
     public static <T> T[] swapElements(T... t) {
@@ -41,8 +75,15 @@ public class GenLib {
 
     public static void main(String[] args) {
         GenLib utils = new GenLib();
-        String[] arr = {"b", "c", "d", "e", "f"};
-
-        System.out.println(Arrays.toString(exchangeGroups(2, arr, "f", "g" , "s")));
+        String[] arr = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+        String[] arr2 = {"f", "g" , "s"};
+//        Arrays.toString(exchangeGroups(2, arr, arr2));
+//        System.out.println(arr.toString());
+//        System.out.println(arr2.toString());
+        swapGrp(arr, 2, 5, 7, 9);
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+        //System.out.println(arr.toString());
     }
 }
