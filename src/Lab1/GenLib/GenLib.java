3fcaecfcb8ab3220cece87bb5f7027032ae9eff7
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-09-30 22:51:31 +0300

    GenLib.insert implemented

--- src/Lab1/GenLib/GenLib.java
+++ src/Lab1/GenLib/GenLib.java
@@ -35,28 +35,18 @@ public class GenLib<T> {
         }
     }
 
-    public static<T> void insertIn(){
-
+    public static <T> T[] insert(T[] from, int fromPos, T[] to, int toPos, int num) throws WrongUsage{
+        if(fromPos + num > from.length) {
+            throw new WrongUsage();
+        }
+        T[] temp = Arrays.copyOf(to, to.length + num);
+        System.arraycopy(from, fromPos, temp, toPos, num);
+        System.arraycopy(to, toPos, temp, toPos + num, to.length - toPos);
+        return temp;
     }
 
     public static<T> void replOtherArr(){
 
-    }
-
-    public static <T> T[] addGroup(int i, T[] in, T... t) {             ///[1,2,3,4,5] +(2)[6,7,8] = [1,2,6,7,8,4,5]
-        Object[] temp = new Object[in.length + t.length];
-        System.arraycopy(in, 0, temp, 0, i);
-        System.arraycopy(t, 0, temp, i, t.length);
-        System.arraycopy(in, i, temp, i + t.length, in.length - i);
-        return (T[]) temp;
-    }
-
-    public static <T> T[] exchangeGroups(int start, T[] in, T... t) {
-        Object[] temp = new Object[in.length];
-        System.arraycopy(in, 0, temp, 0, start); // [1,2,3,4,5,6,7,8]+(2)[9.10,11]=[1,2,9,10,11,6,7,8]
-        System.arraycopy(t, 0, temp, start, t.length);
-        System.arraycopy(in, start + t.length, temp, start + t.length, in.length - (start + t.length));
-        return (T[]) temp;
     }
 
     public void print(T[] arr){
@@ -78,6 +68,12 @@ public class GenLib<T> {
         }
         utils.print(arr1);
         swapNeighb(arr1);
+        utils.print(arr1);
+        try {
+            arr1 = insert(arr2, 3, arr1, 4, 3);
+        }catch(WrongUsage wr) {
+            System.out.println(wr.getMessage());
+        }
         utils.print(arr1);
     }
 }
