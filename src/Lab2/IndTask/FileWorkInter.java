6ab27bd0de39b91cfa681f408f47e8e4a0324ae6
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-11-07 19:08:05 +0200

    IndTask: TextFileDoctor implemented, XMLDoctor created

---
+++ src/Lab2/IndTask/FileWorkInter.java
@@ -0,0 +1,6 @@
+package Lab2.IndTask;
+
+public interface FileWorkInter {
+    void fileRead(String filename);
+    void fileWrite(String filename);
+}
---
+++ src/Lab2/IndTask/XMLDoctor.java
@@ -0,0 +1,17 @@
+package Lab2.IndTask;
+
+import Lab1.Ind_task.DoctorArr;
+
+public class XMLDoctor extends DoctorArr implements FileWorkInter {
+    public void fileRead(String filename){
+
+    }
+
+    public void fileWrite(String filename){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
