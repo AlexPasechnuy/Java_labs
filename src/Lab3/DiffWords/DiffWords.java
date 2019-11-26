d73e6970c81596857e97e7341d71d39a45c52956
AlexPasechnuy <alexpasechnuy@gmail.com>
2019-11-26 22:33:22 +0200

    Lab3.DiffWords implemented

---
+++ src/Lab3/DiffWords/DiffWords.java
@@ -0,0 +1,36 @@
+package Lab3.DiffWords;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class DiffWords {
+    Set<String> words = new TreeSet<>();
+
+    DiffWords(String str){
+        stringToSet(str);
+    }
+
+    public void read(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        stringToSet(str);
+    }
+
+    public void stringToSet(String str){
+        StringTokenizer st = new StringTokenizer(str);
+        while(st.hasMoreTokens()){
+            words.add(st.nextToken());
+        }
+    }
+
+    public Set getSet(){
+        return words;
+    }
+
+    public static void main(String[] args) {
+        DiffWords wrds = new DiffWords("hello my dear friend");
+        System.out.println(wrds.getSet());
+    }
+}
