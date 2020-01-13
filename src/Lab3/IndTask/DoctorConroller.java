20782a1d3e1cfac819f0de2b31627c6efaa5c312
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-01-13 21:09:29 +0200

    Facade with singletone implemented

--- src/Lab3/IndTask/DoctorConroller.java
+++ src/Lab3/IndTask/DoctorConroller.java
@@ -27,7 +27,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class DoctorConroller implements Initializable {
-    List<DoctorArr> doctors = new ArrayList<>();
+    private DoctorFacade facade = DoctorFacade.getInstance();
 
     public TextField surnSearch;
     public TextField dateSearch;
@@ -93,13 +93,6 @@ public class DoctorConroller implements Initializable {
             return count.get();}
     }
 
-    public List<DoctorArr> getDoctors(){
-        if(doctors == null){
-            doctors = new ArrayList<>();
-        }
-        return doctors;
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         docTableInit();
@@ -108,7 +101,7 @@ public class DoctorConroller implements Initializable {
     private void docTableInit() {
         //initializing rows
         ObservableList<TableRow> rows = FXCollections.observableArrayList();
-        for (DoctorArr dct : doctors) {
+        for (DoctorArr dct : facade.getDoctors()) {
             rows.add(new TableRow(dct.getSurn(), dct.getSpec(), dct.getRec(0).getDay(),
                     dct.getRec(0).getShift(), dct.getRec(0).getCount()));
             for (int i = 1; i < dct.getLength(); i++) {
@@ -124,38 +117,9 @@ public class DoctorConroller implements Initializable {
         countCol.setCellValueFactory(new PropertyValueFactory<>("count"));
     }
 
-    private void readFromFile(String path){
-        doctors = new ArrayList<>();
-        try {
-            Document doc;
-            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            doc = db.parse(new File(path));
-            Node rootNode = doc.getDocumentElement();
-            mainLoop:
-            for (int i = 0; i < rootNode.getChildNodes().getLength(); i++) {
-                Node doctorNode = rootNode.getChildNodes().item(i);
-                if (doctorNode.getNodeName().equals("Doctor")) {
-                    DoctorArr doctor = new DoctorArr(doctorNode.getAttributes().getNamedItem("Surname").getNodeValue(),
-                            doctorNode.getAttributes().getNamedItem("Speciality").getNodeValue());
-                    for (int j = 0; j < doctorNode.getChildNodes().getLength(); j++) {
-                        Node reception = doctorNode.getChildNodes().item(j);
-                        if (reception.getNodeName().equals("Reception")) {
-                            doctor.addRec(new Reception(reception.getAttributes().getNamedItem("Date").getNodeValue(),
-                                    Integer.parseInt(reception.getAttributes().getNamedItem("Shift").getNodeValue()),
-                                    Integer.parseInt(reception.getAttributes().getNamedItem("Count").getNodeValue())));
-                        }
-                    }
-                    getDoctors().add(doctor);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @FXML
     private void doNew(javafx.event.ActionEvent event) {
-        doctors = new ArrayList<>();
+        facade.doNew();
         docTableInit();
     }
 
@@ -165,9 +129,7 @@ public class DoctorConroller implements Initializable {
         File file;
         if ((file = fileChooser.showOpenDialog(null)) != null) {
             try {
-                readFromFile(file.getCanonicalPath());
-                docTableInit();
-                docTable.setItems(null);
+                facade.doOpen(file.getCanonicalPath());
                 docTableInit();
             } catch (IOException e) {
                 showError("No such file");
@@ -181,7 +143,7 @@ public class DoctorConroller implements Initializable {
         File file;
         if ((file = fileChooser.showSaveDialog(null)) != null) {
             try {
-                writeToFile(file.getCanonicalPath());
+                facade.doSave(file.getCanonicalPath());
                 showMessage("Results are saved");
             } catch (Exception e) {
                 showError("Error write to file");
@@ -191,68 +153,32 @@ public class DoctorConroller implements Initializable {
 
     @FXML
     private void doAdd(javafx.event.ActionEvent event){
-        if(surnAdd.getText().isEmpty() || specAdd.getText().isEmpty() || dateAdd.getText().isEmpty()
-                || shiftAdd.getText().isEmpty()|| countAdd.getText().isEmpty()){
-            throw new NullPointerException();
-        }
-        DoctorArr doc = new DoctorArr(surnAdd.getText(), specAdd.getText());
-        for(DoctorArr dct : getDoctors()){
-            if(((doc.getSurn().indexOf(dct.getSurn()))==0 && doc.getSurn().length() == dct.getSurn().length())
-                    &&( (doc.getSpec().indexOf(dct.getSpec()))==0&& doc.getSpec().length() == dct.getSpec().length())){
-                try {
-                    dct.addRec(new Reception(dateAdd.getText(), Integer.parseInt(shiftAdd.getText())
-                            , Integer.parseInt(countAdd.getText())));
-                }catch (WrongUsage ex){
-                    showError("Wrong shift or count");
-                    return;
-                }
-                docTableInit();
-                return;
-            }
-        }
-        try{
-        doc.addRec(new Reception(dateAdd.getText(), Integer.parseInt(shiftAdd.getText())
-                , Integer.parseInt(countAdd.getText())));
-        }catch (WrongUsage ex){
+        try {
+            facade.doAdd(surnAdd.getText(), specAdd.getText(), dateAdd.getText(), shiftAdd.getText(), countAdd.getText());
+        }catch(WrongUsage ex){
             showError("Wrong shift or count");
-            return;
         }
-        getDoctors().add(doc);
         docTableInit();
     }
 
     @FXML
     private void doSearch(javafx.event.ActionEvent event) {
-        String result = "";
-        if(surnSearch.getText().isEmpty() || dateSearch.getText().isEmpty()){
-            throw new NullPointerException();
+        try {
+            searchRes.setText(facade.doSearch(surnSearch.getText(), dateSearch.getText()));
+        }catch (NullPointerException ex){
+            showError("Some field is empty");
         }
-        for(DoctorArr doc : getDoctors()){
-            if(doc.getSurn().indexOf(surnSearch.getText().toString()) == 0 && doc.getSurn().length() == surnSearch.getText().length()){
-                result +=doc.getSurn() + ", " + doc.getSpec() + ":\n";
-                AbsRecept[] arr =  doc.searchByDay(dateSearch.getText());
-                for(AbsRecept rec : arr){
-                    System.out.println(rec.toString());
-                    result += "     " + rec.toString() + '\n';
-                }
-            }
-        }
-        searchRes.setText(result);
     }
 
     @FXML
     private void doDateSort(javafx.event.ActionEvent event) {
-        for(DoctorArr doc : getDoctors()){
-            doc.sortByDay();
-        }
+        facade.doDateSort();
         docTableInit();
     }
 
     @FXML
     private void doCountSort(javafx.event.ActionEvent event) {
-        for(DoctorArr doc : getDoctors()){
-            doc.sortByVisCount();
-        }
+        facade.doCountSort();
         docTableInit();
     }
 
@@ -286,40 +212,5 @@ public class DoctorConroller implements Initializable {
         alert.setHeaderText("Application shows information about doctors and their receptions");
         alert.setContentText("Version 1.0");
         alert.showAndWait();
-    }
-
-    private void writeToFile(String path){
-        try {
-            SimpleDateFormat ft = new SimpleDateFormat ("dd.MM.yyyy");
-            DocumentBuilderFactory dbf;
-            DocumentBuilder db;
-            Document doc;
-
-            dbf = DocumentBuilderFactory.newInstance();
-            db = dbf.newDocumentBuilder();
-            doc = db.newDocument();
-
-            Element dcts = doc.createElement("Doctors");
-            for(DoctorArr dct: getDoctors()){
-                Element xmlDoc = doc.createElement("Doctor");
-                xmlDoc.setAttribute("Surname", dct.getSurn());
-                xmlDoc.setAttribute("Speciality", dct.getSpec());
-                for(int i = 0; i < dct.getLength(); i++){
-                    Element rec = doc.createElement("Reception");
-                    rec.setAttribute("Date",ft.format(dct.getRec(i).getDay()));
-                    rec.setAttribute("Shift",Integer.toString(dct.getRec(i).getShift()));
-                    rec.setAttribute("Count",Integer.toString(dct.getRec(i).getCount()));
-                    xmlDoc.appendChild(rec);
-                }
-                dcts.appendChild(xmlDoc);
-            }
-            doc.appendChild(dcts);
-
-            Transformer transformer = TransformerFactory.newInstance().newTransformer();
-            transformer.transform(new DOMSource(doc),
-                    new StreamResult(new FileOutputStream(new File(path))));
-        }catch (Exception ex){
-            ex.printStackTrace();
-        }
     }
 }
